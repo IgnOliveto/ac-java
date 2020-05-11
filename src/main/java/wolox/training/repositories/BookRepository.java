11990f918d939682e79d209795b21d75f674f554
Andrea <andrea.celtich@wolox.com.ar>
2020-05-11 19:51:04 -0300

    added crud methods

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,5 +1,7 @@
 package wolox.training.repositories;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
@@ -7,6 +9,10 @@ import wolox.training.models.Book;
 
 @EnableJpaRepositories
 @Repository
-public interface BookRepository extends JpaRepository<Book, String> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    void deleteById(Long id);
 }
