e5e80742276f8308a1391fa14a503a7e24ced102
Andrea <andrea.celtich@wolox.com.ar>
2020-05-12 17:29:12 -0300

    added cascade to one to many relation to fix bug

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -4,15 +4,18 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import wolox.training.exceptions.BookAlreadyOwnedException;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -25,7 +28,7 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Book> books;
 
     @Column(nullable = false)
