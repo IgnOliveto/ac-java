116ea030834c4cd94c72cfea2e9930fb57d1daaf
Andrea <andrea.celtich@wolox.com.ar>
2020-05-12 14:05:41 -0300

    added reasons for http response

--- src/main/java/wolox/training/exceptions/BookIdMismatchException.java
+++ src/main/java/wolox/training/exceptions/BookIdMismatchException.java
@@ -3,7 +3,7 @@ package wolox.training.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Body and params data do not match.")
 public class BookIdMismatchException extends RuntimeException {
     public BookIdMismatchException(Long bookId, Long paramId) {
         super("The book ID " + bookId + " and the parameter id " + paramId + " do not match.");
--- src/main/java/wolox/training/exceptions/IsbnDoesNotBelongToAnyBookException.java
+++ src/main/java/wolox/training/exceptions/IsbnDoesNotBelongToAnyBookException.java
@@ -3,7 +3,7 @@ package wolox.training.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "ISBN does not match with any book.")
 public class IsbnDoesNotBelongToAnyBookException extends RuntimeException{
     public IsbnDoesNotBelongToAnyBookException(String isbn) {
         super( "The book with ISBN " + isbn + " does not exist.");
